627ad043bf8553ee04c8ca362548157d430d4778
vercherebrian <brian.verchere@iut-dijon.u-bourgogne.fr>
2024-02-16 11:37:50 +0100

    Create Player class

---
+++ src/main/java/org/example/rpgapi/models/Player.java
@@ -0,0 +1,63 @@
+package org.example.rpgapi.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="player")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private int level;
+
+    private int experience;
+
+    private int health;
+
+    public Player() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
