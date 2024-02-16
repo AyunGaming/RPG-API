70702103683d659e3d0379f51e0a29932d4ac223
vercherebrian <brian.verchere@iut-dijon.u-bourgogne.fr>
2024-02-16 14:57:53 +0100

    Add APIExceptionHandler class for exception handle + add missing space in PlayerNotFoundException message

--- src/main/java/org/example/rpgapi/errors/PlayerNotFoundException.java
+++ src/main/java/org/example/rpgapi/errors/PlayerNotFoundException.java
@@ -2,6 +2,6 @@ package org.example.rpgapi.errors;
 
 public class PlayerNotFoundException extends ObjectNotFoundException{
     public PlayerNotFoundException(int id) {
-        super("Player with id " + id + "not found.");
+        super("Player with id " + id + " not found.");
     }
 }
