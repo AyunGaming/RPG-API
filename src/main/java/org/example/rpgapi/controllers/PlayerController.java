eaf42b5de3551585bf99b93be47da15d623725ef
vercherebrian <brian.verchere@iut-dijon.u-bourgogne.fr>
2024-02-16 11:46:48 +0100

    Add PlayerController.java documentation

--- src/main/java/org/example/rpgapi/controllers/PlayerController.java
+++ src/main/java/org/example/rpgapi/controllers/PlayerController.java
@@ -6,15 +6,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller for player related actions
+ */
 @RestController
 public class PlayerController {
 
     private final PlayerService service;
 
+    /**
+     * Initialize the service
+     * @param service the PlayerService
+     */
     public PlayerController(PlayerService service){
         this.service = service;
     }
 
+    /**
+     * Create a new player
+     * @param player the player to create (passed in the body of the request)
+     * @return the player created
+     */
     @PostMapping("/create/player")
     public Player createPlayer(@RequestBody Player player){
         return service.createPlayer(player);
