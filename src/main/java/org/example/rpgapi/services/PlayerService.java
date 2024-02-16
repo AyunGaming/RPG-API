b04c82b071020481734b5843e15c8c993b55a52e
vercherebrian <brian.verchere@iut-dijon.u-bourgogne.fr>
2024-02-16 11:50:31 +0100

    Add PlayerService.java documentation

--- src/main/java/org/example/rpgapi/services/PlayerService.java
+++ src/main/java/org/example/rpgapi/services/PlayerService.java
@@ -4,6 +4,9 @@ import org.example.rpgapi.models.Player;
 import org.example.rpgapi.repositories.PlayerRepository;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service to access to player's repository
+ */
 @Service
 public class PlayerService {
     private final PlayerRepository repository;
@@ -12,6 +15,11 @@ public class PlayerService {
         this.repository = repository;
     }
 
+    /**
+     * Add a new player in the repository
+     * @param player the player to add
+     * @return the player created
+     */
     public Player createPlayer(Player player) {
         return repository.save(player);
     }
